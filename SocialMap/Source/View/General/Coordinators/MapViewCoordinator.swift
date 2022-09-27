e40b26d4cdcdc78909d60b48e12e97b93f5df570
daneccc <danielecavalcantecf@gmail.com>
2022-09-27 11:52:50 -0300

    add context menu to annotation view

--- SocialMap/Source/View/General/Coordinators/MapViewCoordinator.swift
+++ SocialMap/Source/View/General/Coordinators/MapViewCoordinator.swift
@@ -1,3 +1,4 @@
+import UIKit
 import Foundation
 import MapKit
 
@@ -7,19 +8,45 @@ class MapViewCoordinator: NSObject, MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard !(annotation is MKUserLocation) else { return nil }
-
+        
         if let annotation = annotation as? UserImageAnnotation {
             let annotationView = MKAnnotationView()
             let annotationFrame = CGSize(width: 64, height: 48)
             let annotationImage = annotation.image.resizeImageTo (
                 size: CGSize(width: 84, height: 64)
-            )?.withRoundedCorners(radius: 16)
+            )?.withRoundedCorners(radius: 20)
             
             annotationView.frame.size = annotationFrame
             annotationView.image = annotationImage
             annotationView.layer.cornerRadius = 20
             annotationView.contentMode = .scaleAspectFit
             annotationView.canShowCallout = true
+            
+            let interaction = UIContextMenuInteraction(delegate: self)
+            
+            annotationView.addInteraction(interaction)
+            
+            // cria o botao INFO
+//            let btn = UIButton(type: .detailDisclosure)
+//            annotationView.rightCalloutAccessoryView = btn
+            
+            
+//            let FirstAction = UIAction(title: "First Action"),
+//                handler: {_ in
+//                    print("First action")
+//                })
+//            
+//            let secondAction = UIAction(title: "Second Action",
+//                                        handler: {_ in
+//                print("Second action")
+//            })
+            
+//            let button = UIButton(type: .detailDisclosure)
+//            button.menu = UIMenu(title: "My Title")
+//            button.showsMenuAsPrimaryAction = true
+//            annotationView.canShowCallout = true
+//            annotationView.rightCalloutAccessoryView = button
+            
             
             return annotationView
         }
@@ -28,7 +55,7 @@ class MapViewCoordinator: NSObject, MKMapViewDelegate {
     }
     
     func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
-
+        
     }
     
     @objc func handleLongTapGesture(gestureRecognizer: UILongPressGestureRecognizer) {
@@ -55,4 +82,35 @@ class MapViewCoordinator: NSObject, MKMapViewDelegate {
         }
         
     }
+}
+
+// MARK: - CONTEXT MENU
+extension MapViewCoordinator : UIContextMenuInteractionDelegate {
+
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        
+        guard let annotationView = interaction.view as? MKAnnotationView else {
+            return nil
+        }
+
+        guard let annotation = annotationView.annotation as? UserImageAnnotation else {
+            return nil
+        }
+
+        return UIContextMenuConfiguration(identifier: "\(annotation.id)" as NSCopying, previewProvider: nil) { _ in
+            
+            let favorite = UIAction(title: "Favorite", image: UIImage(systemName: "heart")) { _ in
+                print("Favorite")
+            }
+            let share = UIAction(title: "Share", image: UIImage(systemName: "share")) { _ in
+                print("Share")
+            }
+            
+            return UIMenu(title: annotation.title ?? "", image: nil, identifier: nil, options: [], children: [favorite, share])
+            
+        }
+    }
+    
+
+
 }
